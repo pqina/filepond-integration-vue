d9dd69911fdfa6cd34421428bf7d1c5c4803e492
Rik <rik@pqina.nl>
2026-05-27 10:46:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# FilePond Integration with Vue
+
+This template should help get you started developing with [FilePond](https://filepond.com) on [Vue](https://vuejs.org/) in [Vite](https://vite.dev).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// filepond imports
+import { defineFilePond } from "filepond";
+import { locale } from "filepond/locales/en-gb.js";
+
+// defines <file-pond> element
+defineFilePond({
+  locale,
+});
+
+function handleChange(e: Event) {
+  console.log("change", e);
+}
+
+function handleSubmit(e: Event) {
+  e.preventDefault();
+  console.log("submit", e);
+}
+</script>
+
+<template>
+  <form @submit="handleSubmit" method="POST">
+    <label for="my-file">Documents</label>
+
+    <file-pond @change="handleChange">
+      <input id="my-file" type="file" name="files" required multiple />
+    </file-pond>
+
+    <button type="submit">Upload</button>
+  </form>
+</template>
+
+<style scoped></style>
