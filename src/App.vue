6ee5e8c6344483a1f7a1986c1b2c61434b12427b
Rik <rik@pqina.nl>
2026-05-29 12:01:38 +0200

    add types

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,51 @@
 <script setup lang="ts">
 // FilePond imports
-import { defineFilePond } from "filepond";
-import { locale } from "filepond/locales/en-gb.js";
+import { defineFilePond, type FilePondEntry } from 'filepond';
+import { locale } from 'filepond/locales/en-gb.js';
+
+// Optionally import Vue <file-pond> component types
+import 'filepond/types/vue';
 
 // Defines <file-pond> custom element
 defineFilePond({
-  locale,
+    locale,
 });
 
-function handleChange(e: Event) {
-  console.log("change", e);
+function handleEntriesChange(e: CustomEvent<FilePondEntry[]>) {
+    console.log('entrieschange', e);
 }
 
 function handleSubmit(e: Event) {
-  e.preventDefault();
-  console.log("submit", e);
+    e.preventDefault();
+    console.log('submit', e);
 }
 </script>
 
 <template>
-  <form @submit="handleSubmit" method="POST">
-    <label for="my-file">Documents</label>
+    <form @submit="handleSubmit" method="POST">
+        <label for="my-file">Documents</label>
 
-    <file-pond @change="handleChange">
-      <input id="my-file" type="file" name="files" required multiple />
-    </file-pond>
+        <file-pond @entrieschange="handleEntriesChange" animations="auto">
+            <input id="my-file" type="file" name="files" required multiple />
+        </file-pond>
 
-    <button type="submit">Upload</button>
-  </form>
+        <button type="submit">Upload</button>
+    </form>
 </template>
 
 <style scoped>
 form {
-  font-family:
-    system-ui,
-    -apple-system,
-    BlinkMacSystemFont,
-    "Segoe UI",
-    Roboto,
-    Oxygen,
-    Ubuntu,
-    Cantarell,
-    "Open Sans",
-    "Helvetica Neue",
-    sans-serif;
+    font-family:
+        system-ui,
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        Oxygen,
+        Ubuntu,
+        Cantarell,
+        'Open Sans',
+        'Helvetica Neue',
+        sans-serif;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
+// Configuring Vite
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    // More info about Vue and web components:
+                    // https://vuejs.org/guide/extras/web-components
+                    isCustomElement: (tag) => ['file-pond'].includes(tag),
+                },
+            },
+        }),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
-})
+});
